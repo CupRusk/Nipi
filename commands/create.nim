c70995c1c03575c1974f3a575615a92f3f4ef0d8
Denis <msoxrap857@gmail.com>
2025-11-11 10:52:12 +0200

    Fix any problems in syntax

--- commands/create.nim
+++ commands/create.nim
@@ -1,29 +1,29 @@
 import ./create_proj/ServiceCreate as cp
 import os, strutils, terminal
 
-proc printf(args: string) =
+proc print(args: string) =
   stdout.write(args)
   stdout.flushFile()
 
 proc new_proj*() =
-  printf("Create...\n")
+  print("Create...\n")
   sleep(500)
 
-  printf("Name project: ")
+  print("Name project: ")
   let name_proj = readLine(stdin)
-  printf("\n")
+  print("\n")
+
   if name_proj.len != 0:
     cp.init_venv(name_proj)
   else:
     cp.init_venv("venv")
 
-
-  printf("Compilation (1 - binary, 2 - cpp, 3 - c, 4 - js, all - all): ")
+  print("Compilation (1 - binary, 2 - cpp, 3 - c, 4 - js, all - all): ")
   let comp = readLine(stdin)
-  printf("\n")
+  print("\n")
   
   var compSplit = comp.split(",")
-  if len(compSplit) != 0:
+  if compSplit.len != 0:
     echo "Selected compilation modes: ", compSplit.join(", ")
     cp.create_bash(comp)
   else:
