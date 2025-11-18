e64bfdb4e6faa0ea5c1885aef756d429872dce9c
Denis <msoxrap857@gmail.com>
2025-11-18 15:14:41 +0200

    rename funtion

--- commands/create.nim
+++ commands/create.nim
@@ -1,26 +1,23 @@
 import ./create_proj/ServiceCreate as cp
 import os, strutils, terminal
 
-proc print(args: string) =
+proc writeLine(args: string) =
   stdout.write(args)
   stdout.flushFile()
 
 proc new_proj*() =
-  print("Create...\n")
-  sleep(500)
-
-  print("Name proj: ")
+  writeLine("Name proj: ")
   let name_proj = readLine(stdin)
-  print("\n")
+  writeLine("\n")
 
   if name_proj.len != 0:
     cp.init_venv(name_proj)
   else:
     cp.init_venv("venv")
 
-  print("Compilation (1 - binary, 2 - cpp, 3 - c, 4 - js, all - all): ")
+  writeLine("Compilation (1 - binary, 2 - cpp, 3 - c, 4 - js, all - all): ")
   let comp = readLine(stdin)
-  print("\n")
+  writeLine("\n")
   
   var compSplit = comp.split(",")
   if compSplit.len != 0:
